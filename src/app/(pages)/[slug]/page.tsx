3721bc6ac48920a08f58f017aa7a3fbec2dca24c
Daniel <pepelacon@gmail.com>
2024-02-12 17:43:30 -0500

    create categories

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -20,6 +20,7 @@ import { Gutter } from '../../_components/Gutter'
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
 import classes from './index.module.scss'
+import Categories from '../../_components/Categories'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -59,7 +60,9 @@ export default async function Page({ params: { slug = 'home' } }) {
       {slug === 'home' ? (
         <section>
           <Hero {...hero} />
-          <Gutter>{/* {categories} */}</Gutter>
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
+          </Gutter>
         </section>
       ) : (
         <>
